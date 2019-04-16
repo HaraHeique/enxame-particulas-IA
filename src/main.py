f6e9ddb6e10e8de3d930f420538e5351cf8a99ad
Harã Heique dos Santos <heikacademicos@hotmail.com>
2019-04-16 00:45:31 -0300

    Estrutura base do código

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# import libraries.userInput as userInput
+# import libraries.utilities.matricesHandle as mtzHandle
+# import libraries.pathfindingHeuristic as heuristic
+import os
+
+def main() :
+    
+
+    return 0
+
+if __name__ == '__main__':
+    main()
